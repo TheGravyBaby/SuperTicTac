54de32f1cc77f3502e7e26688a467c0afe974e17
Andrew Argraves <aargraves@berkeley.edu>
2020-08-09 22:18:53 -0400

    MONTE WORKS!  But recursion causes stack overflow

--- superTicTacMonte.py
+++ superTicTacMonte.py
@@ -6,25 +6,27 @@ import math as m
 import random
 
 # determines the number of playouts the algorithm will attempt before making a descision
-playouts = 100
+
+player = 1
+global_playouts = 0
+allowed_playouts = 100
 
 class monteNode(): 
-    def __init__(self, GameNode, Parent, ParentPlayouts):
+    def __init__(self, GameNode, Parent):
         self.GameNode = GameNode
         self.Parent = Parent
         self.Children = []
-        self.ParentPlayouts = ParentPlayouts
         self.ChildPlayouts = 0
         self.WonPlayouts = 0
         self.UCB1 = m.inf
         
     def calculateUCB1(self):
-        self.UCB1 = self.WonPlayouts / self.ChildPlayouts + m.pow(2, .5) * m.pow(m.log(self.ParentPlayouts) , .5)
+        self.UCB1 = self.WonPlayouts / self.ChildPlayouts + m.pow(2, .5) * m.pow(m.log(global_playouts) / self.ChildPlayouts, .5)
         # after a won playout compute UBC
  
 # 1. Create the root monte node... (could be a root or any node really)
 def monte_initiate(GameNode):   
-    root_node =  monteNode(GameNode, None, 0)
+    root_node =  monteNode(GameNode, None)
     monte_expand(root_node)
 
     
@@ -36,24 +38,31 @@ def monte_expand(node):
         # make new child nodes
         for move in node.GameNode.validMoves : 
            # newGameState = makeMoveNewBoard(node.GameNode, move)
-            node.Children.append(monteNode(makeMoveNewBoard(node.GameNode, move), node, node.ParentPlayouts))       
+            node.Children.append(monteNode(makeMoveNewBoard(node.GameNode, move), node))       
     monte_select(node)
 
 
 # 3. SELECT! Pick a node to keep traveling down
 def monte_select(node):
+
+    global global_playouts
     
     # check for draws on this node
     if node.GameNode.winState == 0 and len(node.GameNode.validMoves) == 0 :
         printGameState(node.GameNode)
-        print("Found a draw")
-        input()
+        global_playouts += 1
+        print("Found a draw")    
+        backpropogate_node(node, 0)
     
     # check for wins     
     elif node.GameNode.winState != 0 :
         printGameState(node.GameNode)
+        global_playouts += 1
         print("Found a win")
-        input()
+        if node.GameNode.winState == player :
+            backpropogate_node(node, 1)
+        else : 
+             backpropogate_node(node, -1)
 
 
     else : 
@@ -74,9 +83,23 @@ def monte_select(node):
 
 # 4. Repeat 2, for all the valid moves make child nodes 
 
-# def backpropogate_node(node):
-#     print("Backpropogate")
-    
+def backpropogate_node(node, winloss):
+    # global global_playouts
+
+    node.WonPlayouts += winloss
+    node.ChildPlayouts += 1
+    node.calculateUCB1()
+
+    # this would be the root node 
+    if node.Parent != None :
+        backpropogate_node(node.Parent, winloss)
+    else :
+        print("Got back to the root")
+        if global_playouts < allowed_playouts :
+            monte_select(node)
+
+
+
 def runAMonte():
     initial_board = [ [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9]
     initial_history = []
